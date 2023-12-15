9ab3236bfbdc334c517ed4e03a834a0e3cbe7ec2
fikretzeybek <fzeybek1972@gmail.com>
2023-12-15 20:08:56 +0300

    first

---
+++ src/test/java/tests/SlaytTest/TO_Form209.java
@@ -0,0 +1,15 @@
+package tests.SlaytTest;
+
+public class TO_Form209 {
+
+    public void formTesti(){
+
+        // //1 - https://testotomasyonu.com/form adresine gidin
+        //        //2- Dogum tarihi gun seçeneğinden index kullanarak 5’i secin
+        //        //3- Dogum tarihi ay seçeneğinden value kullanarak Nisan’i secin
+        //        //4- Dogum tarihi yil seçeneğinden visible text kullanarak 1990’i secin
+        //        //5- Secilen değerleri konsolda yazdirin
+        //        //6- Ay dropdown menüdeki tum değerleri(value) yazdırın
+        //        //7- Ay Dropdown menusunun boyutunun 13 olduğunu test edin
+    }
+}
---
+++ src/test/java/tests/SlaytTest/TO_Form210.java
@@ -0,0 +1,16 @@
+package tests.SlaytTest;
+
+public class TO_Form210 {
+
+    public void formTesti(){
+
+        //1 - https://testotomasyonu.com/form adresine gidin
+        //2- Sirt Agrisi ve Carpinti checkbox’larini secin
+        //3- Sirt Agrisi ve Carpinti checkbox’larininin seçili
+        //olduğunu test edin
+        //4- Seker ve Epilepsi checkbox’larininin seçili
+        //olmadigini test edin
+        //5- sayfayi kapatin
+
+    }
+}
