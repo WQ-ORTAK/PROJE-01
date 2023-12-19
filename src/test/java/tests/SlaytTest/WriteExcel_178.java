1f44eadc5407272aa11d4f084c4b3ace09ec0d33
fikretzeybek <fzeybek1972@gmail.com>
2023-12-19 09:22:38 +0300

    ReusableMethods

--- src/test/java/tests/SlaytTest/WriteExcel_178.java
+++ src/test/java/tests/SlaytTest/WriteExcel_178.java
@@ -1,8 +1,17 @@
 package tests.SlaytTest;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class WriteExcel_178{
 
-    public void test01(){
+    public void test01() throws IOException {
         //1) Yeni bir Class olusturalim WriteExcel
         //2) Yeni bir test method olusturalim writeExcelTest()
         //3) Adimlari takip ederek Sayfa1’deki1.satira kadar gidelim
@@ -13,6 +22,8 @@ public class WriteExcel_178{
         //8) 15.satir nufus kolonuna 54000 yazdiralim
         //9) Dosyayi kaydedelim
         //10)Dosyayi kapatalim
-    }
+        //2) Yeni bir test method olusturalim writeExcelTest()
+        //3) Adimlari takip ederek Sayfa1’deki 1.satira kadar gidelim
 
+    }
 }
--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -132,7 +132,7 @@ public class ReusableMethods {
     }
 
     //   HARD WAIT WITH THREAD.SLEEP
-        //   waitFor(5);  => waits for 5 second
+//   waitFor(5);  => waits for 5 second
     public static void waitFor(int sec) {
         try {
             Thread.sleep(sec * 1000);
@@ -206,4 +206,17 @@ public class ReusableMethods {
     }
 
 
+    public static String getScreenshot(String name) throws IOException {
+        // naming the screenshot with the current date to avoid duplication
+        String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        // TakesScreenshot is an interface of selenium that takes the screenshot
+        TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        // full path to the screenshot location
+        String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name + date + ".png";
+        File finalDestination = new File(target);
+        // save the screenshot to the path given
+        FileUtils.copyFile(source, finalDestination);
+        return target;
+    }
 }
